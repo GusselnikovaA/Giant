8fd5223e4ad8a25bcc87a5a427c315d467c27a71
GusselnikovaA <annanac1993@gmail.com>
2020-05-18 12:33:28 +0300

    Подключение jquery и slick слайдера. Сохранение картинок в png

---
+++ src/js/sliders-setting.js
@@ -0,0 +1,33 @@
+// слайдер в блоке отзывов (feedback) 
+$('.feedback-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  speed: 2000,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  infinite: true,
+  speed: 2000,
+  prevArrow: $('.feedback__arrows_left'),
+  nextArrow: $('.feedback__arrows_right'),
+  // responsive: [{
+  //     breakpoint: 1350,
+  //     settings: {
+  //       slidesToShow: 3
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 880,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       arrows: false
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 500,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       dots: false
+  //     }
+  //   }
+  // ]
+});
